79593873e6f38c8f37a6abc8b8c347c8fc3115da
egabrielse <egabrielse11@gmail.com>
2020-10-20 13:41:24 -0500

    Initial Commit

---
+++ MyCameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCameraApp
+//
+//  Created by Ethan Gabrielse on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
